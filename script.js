12eae58a8193e6c7686c19d4eb3e33b331652c85
abbycute27 <cabbalda22@src.pshs.edu.ph>
2025-02-12 05:27:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
                let time = parseFloat(prompt('Enter time in hours: '));
  
                finalPop = initialPop*(Math.pow(Math.E,(growthRate*time)));
-			   let roundedResult = Math.round(finalPop);
+	       let roundedResult = Math.round(finalPop);
  
-			   let monsterLocation = prompt('Enter the region/location of the monster: ');
-			   let monsterName = prompt('Enter the name of the monster:');  
-			   let sigmaMonster = monsterLocation.concat ( " " + monsterName).toUpperCase();
+	       let monsterLocation = prompt('Enter the region/location of the monster: ');
+	       let monsterName = prompt('Enter the name of the monster:');  
+	       let sigmaMonster = monsterLocation.concat ( " " + monsterName).toUpperCase();
      
               document.getElementById('result').innerHTML =('After ' + time + ' hours, ' + ' the population of ' + sigmaMonster + ' has increased to ' + roundedResult +"!");
 }
